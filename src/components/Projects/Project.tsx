d43d568e6bbf0d8e1966161da5bfcc4d8fc33807
Carol Silva <contatocarolinebs@gmail.com>
2025-03-25 22:28:03 -0300

    fix: remove props

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,6 +1,6 @@
 import { typeProject } from "../../types/types";
 
-const Project = ({projectName, description, imgSource, tags, demo, github}: typeProject) => {
+const Project = ({projectName, description, tags}: typeProject) => {
   return (
     <div className="rounded-lg border text-card-foreground shadow-sm bg-zinc-900 border-zinc-800 overflow-hidden max-w-sm">
       <div className="aspect-video relative">
@@ -45,11 +45,11 @@ const Project = ({projectName, description, imgSource, tags, demo, github}: type
             >
               <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
               <path d="M9 18c-4.51 2-5-2-7-2"></path>
-            </svg>{" "}
+            </svg>
             Code
           </button>
           <button className="flex items-center justify-center bg-purple-500 text-primary-foreground hover:bg-purple-300 h-9 rounded-md px-3 gap-1 transition duration-500 ease-in-out hover:scale-110">
-            Live Demo{" "}
+            Live Demo
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
